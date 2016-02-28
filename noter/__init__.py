bf17c8f7dbd2a9f4685e3071f0a30dd917825bc7
Miguel <mige@airmail.cc>
2016-02-27 23:54:26 -0500

    date support

--- noter/__init__.py
+++ noter/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -9,13 +10,13 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     body = db.Column(db.Unicode)
-    date = db.Column(db.DateTime)
+    post_date = db.Column(db.Text)
 
     def __init__(self, title, body, post_date=None):
         self.title = title
         self.body = body
         if post_date is None:
-            post_date = datetime.utcnow()
+            self.post_date = datetime.date.today()
 
     def __repr__(self):
         return '<title %r>' % self.title
