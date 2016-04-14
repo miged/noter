cffbc970411a7d5667135d9c64650aa6fd9b458f
Miguel <mige@airmail.cc>
2016-04-13 20:54:19 -0400

    Fix indenting

--- noter/models.py
+++ noter/models.py
@@ -4,21 +4,21 @@ from sqlalchemy.ext.hybrid import hybrid_property
 import datetime
 
 class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text)
-    body = db.Column(db.Unicode)
-    post_date = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.Text)
+	body = db.Column(db.Unicode)
+	post_date = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body, user, post_date=None):
-        self.title = title
-        self.body = body
-        self.user_id = user
-        if post_date is None:
-            self.post_date = datetime.date.today()
+	def __init__(self, title, body, user, post_date=None):
+		self.title = title
+		self.body = body
+		self.user_id = user
+		if post_date is None:
+			self.post_date = datetime.date.today()
 
-    def __repr__(self):
-        return '<title %r>' % self.title
+	def __repr__(self):
+		return '<title %r>' % self.title
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
